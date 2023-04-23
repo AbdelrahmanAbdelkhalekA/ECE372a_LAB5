090cac94f51f06729008a33f70a94ed8d5dc89c1
davilap1 <112440543+davilap1@users.noreply.github.com>
2023-04-23 14:55:11 -0700

    Now changes display based on z_axis value
    Added state machine for the display and now changes based on the z-axis value of the accelerometer.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,12 @@
 #define SL_TEMP_LOW       0x42
 #define SL_WHO_AM_I       0X75
 
+typedef enum stateEnum {wait_press, debounce_press, wait_release, debounce_release} stateType;
+volatile stateType state = wait_press;
+
+typedef enum angleEnum {happy_angle, happy_2_sad, sad_2_happy, sad_angle} angleType;
+volatile angleType angle = happy_angle;
+
 
 int main () {
 
@@ -30,7 +36,6 @@ int main () {
   SPI_MASTER_Init(); // initialize SPI module and set the data rate
   initI2C();  // initialize I2C and set bit rate
   
-  signed int T_val = 0;
   signed int x_val = 0;
   signed int y_val = 0;
   signed int z_val = 0;
@@ -44,42 +49,14 @@ int main () {
   write_execute(0x0B, 0x07); // scanning all rows and columns
   write_execute(0x0C,0x01); // set shutdown register to normal operation (0x01)
   write_execute(0x0F, 0x00); // display test register - set to normal operation (0x01)
-  
+  StartI2C_Trans(SLA);
   write(PWR_MGMT);// address on SLA for Power Management
   write(WAKEUP); // send data to Wake up from sleep mode
   StopI2C_Trans();
-  writeTo(SLA, ACCEL_CONFIG, AFS_SEL_2);
+//   writeTo(SLA, ACCEL_CONFIG, AFS_SEL_2);
 
   while(1){
-
-    // write "HI" to LED array.  Must address all 8 rows.  
-    //Each row has data for individual LED:   "1" LED is on, "0" LED is off
-
-    write_execute(0x01, 0b00000000); // all LEDS in Row 1 are off
-    write_execute(0x02, 0b01000010); // row 2 LEDS 
-    write_execute(0x03, 0b10100101); // row 3 LEDS
-    write_execute(0x04, 0b01000010); // row 4 LEDS
-    write_execute(0x05, 0b00000000); // row 5 LEDS
-    write_execute(0x06, 0b10000001); // row 6 LEDS
-    write_execute(0x07, 0b01111110); // row 7 LEDS
-    write_execute(0x08, 0b00000000); // row 8 LEDS
-
-    _delay_ms(1000);  // delay for 1 s to display "HI"
-
-    /// write "ALL" to LED array.  Must address all 8 rows.  
-    //Each row has data for individual LED:   "1" LED is on, "0" LED is off
-
-    write_execute(0x01, 0b00000000);
-    write_execute(0x02, 0b01000010);
-    write_execute(0x03, 0b00000000);
-    write_execute(0x04, 0b00000000);
-    write_execute(0x05, 0b00111100);
-    write_execute(0x06, 0b01000010);
-    write_execute(0x07, 0b10000001);
-    write_execute(0x08, 0b00000000);
-
-    _delay_ms(2000); // delay for 2 sec to display "ALL"
-    
+    // #########################################################
     // status = TWSR & 0xF8;
     Read_from(SLA,SL_MEMA_XAX_HIGH);
     x_val= Read_data(); // read upper value
@@ -100,12 +77,12 @@ int main () {
  
     Read_from(SLA,SL_MEMA_ZAX_LOW);
     z_val = (z_val << 8 )| Read_data(); // append lower value
-    z_accel = z_val / 16384.0;
+    z_accel = (z_val / 16384.0) - 0.07;
 
     Read_from(SLA, ACCEL_CONFIG);
     mode = Read_data();
-
-  
+    // #########################################################
+    // -- Prints the values for the x, y, z axis --
     Serial.println(mode);
     Serial.print("X-Axis =  ");
     Serial.print(x_accel);
@@ -114,7 +91,62 @@ int main () {
     Serial.print(",  Z-Axis = ");
     Serial.println(z_accel);
     StopI2C_Trans();
-    
+    // #########################################################
+
+    // ############################################
+    // -- Checks to the state of the angle rolloff --
+    // -- by first checking the state of the angle --
+    // -- (happy_angle if z value is >= 0.75) --
+    // -- (sad_angle if z value is < 0.75) --
+    // -- If the angle is in the happy_angle state and --
+    // -- the z value < 0.75, the state will change to happy_2_sad. --
+    // -- This works the other way as well --
+    if (angle == happy_angle) {
+        if (z_accel < 0.75) {
+            angle = happy_2_sad;
+        }
+    }
+    else if (angle == sad_angle) {
+        if (z_accel >= 0.75) {
+            angle = sad_2_happy;
+        }
+    }
+    // ############################################
+
+    // #########################################################
+    switch (angle) {
+        case happy_angle:
+            break;
+        case happy_2_sad:
+            angle = sad_angle;
+            /// write a smiley face to LED array.  Must address all 8 rows.  
+            //Each row has data for individual LED:   "1" LED is on, "0" LED is off
+            write_execute(0x01, 0b00000000);
+            write_execute(0x02, 0b01000010);
+            write_execute(0x03, 0b00000000);
+            write_execute(0x04, 0b00000000);
+            write_execute(0x05, 0b00111100);
+            write_execute(0x06, 0b01000010);
+            write_execute(0x07, 0b10000001);
+            write_execute(0x08, 0b00000000);
+            _delay_ms(1000); // delay for 2 sec to display "ALL"
+        case sad_angle:
+            break;
+        case sad_2_happy:
+            angle = happy_angle;
+            // write a frowny face to LED array.  Must address all 8 rows.  
+            //Each row has data for individual LED:   "1" LED is on, "0" LED is off
+            write_execute(0x01, 0b00000000); // all LEDS in Row 1 are off
+            write_execute(0x02, 0b01000010); // row 2 LEDS 
+            write_execute(0x03, 0b10100101); // row 3 LEDS
+            write_execute(0x04, 0b01000010); // row 4 LEDS
+            write_execute(0x05, 0b00000000); // row 5 LEDS
+            write_execute(0x06, 0b10000001); // row 6 LEDS
+            write_execute(0x07, 0b01111110); // row 7 LEDS
+            write_execute(0x08, 0b00000000); // row 8 LEDS
+            _delay_ms(1000);  // delay for 1 s to display "HI"
+    }
+    // #########################################################
     
 }
 return 0;
